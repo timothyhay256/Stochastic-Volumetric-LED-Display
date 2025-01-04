8d7081f0e9c5d93e5bc2a6fc295b25feef8dad72
James Hoffman <purpledungeoncrab@protonmail.com>
2025-01-04 15:43:19 -0700

    Add post_process to scan.rs

--- src/gui.rs
+++ src/gui.rs
@@ -1,11 +1,16 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use crate::scan;
+use crate::speedtest;
+use crate::speedtest::speedtest;
+use crate::Config;
+use crate::ManagerData;
 use std::error::Error;
 
 slint::include_modules!();
 
-pub fn main() -> Result<(), Box<dyn Error>> {
+pub fn main(config: Config, mut manager: ManagerData) -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
     // ui.on_request_increase_value({
@@ -15,6 +20,13 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     //         ui.set_counter(ui.get_counter() + 1);
     //     }
     // });
+    ui.on_speedtest(move || {
+        speedtest(&mut manager, config.num_led, 750);
+    });
+
+    ui.on_calibrate(move || {
+        scan::scan(config, &mut manager).unwrap();
+    });
 
     ui.run()?;
 
