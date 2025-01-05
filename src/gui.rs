43c1fa76264b9476243bad13f7894770e44e08b7
James Hoffman <purpledungeoncrab@protonmail.com>
2025-01-05 16:15:23 -0700

    Fix GUI

--- src/gui.rs
+++ src/gui.rs
@@ -2,30 +2,55 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use crate::scan;
-use crate::speedtest;
 use crate::speedtest::speedtest;
 use crate::Config;
 use crate::ManagerData;
 use std::error::Error;
+use std::sync::{Arc, Mutex};
+use std::time::SystemTime;
 
 slint::include_modules!();
 
-pub fn main(config: Config, mut manager: ManagerData) -> Result<(), Box<dyn Error>> {
+pub fn main(config: Config) -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    // ui.on_request_increase_value({
-    //     let ui_handle = ui.as_weak();
-    //     move || {
-    //         let ui = ui_handle.unwrap();
-    //         ui.set_counter(ui.get_counter() + 1);
-    //     }
-    // });
+    // Wrap ManagerData in Arc<Mutex<ManagerData>> for shared, mutable access
+    let shared_manager = Arc::new(Mutex::new(ManagerData {
+        num_led: config.num_led,
+        communication_mode: config.communication_mode,
+        host: config.host,
+        port: config.port,
+        serial_port_path: config.serial_port_path.clone(),
+        baud_rate: config.baud_rate,
+        serial_read_timeout: config.serial_read_timeout,
+        record_data: config.record_data,
+        record_data_file: config.record_data_file.clone(),
+        record_esp_data: config.record_esp_data,
+        unity_controls_recording: config.unity_controls_recording,
+        record_esp_data_file: config.record_esp_data_file.clone(),
+        failures: 0,
+        con_fail_limit: config.con_fail_limit,
+        print_send_back: config.print_send_back,
+        udp_read_timeout: config.udp_read_timeout,
+        first_run: true,
+        call_time: SystemTime::now(),
+        data_file_buf: None,
+        esp_data_file_buf: None,
+        udp_socket: None,
+        serial_port: None,
+        keepalive: true,
+    }));
+
+    let shared_manager_speedtest = Arc::clone(&shared_manager);
     ui.on_speedtest(move || {
+        let mut manager = shared_manager_speedtest.lock().unwrap();
         speedtest(&mut manager, config.num_led, 750);
     });
 
+    let shared_manager_calibrate = Arc::clone(&shared_manager);
     ui.on_calibrate(move || {
-        scan::scan(config, &mut manager).unwrap();
+        let mut manager = shared_manager_calibrate.lock().unwrap();
+        scan::scan(config.clone(), &mut manager).unwrap();
     });
 
     ui.run()?;
