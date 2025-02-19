1b19c1e5f03bec853ec63dab3d2e1dd1af78d408
James Hoffman <purpledungeoncrab@protonmail.com>
2025-02-18 18:22:38 -0700

    WIP Add multi camera support

--- src/gui.rs
+++ src/gui.rs
@@ -1,13 +1,10 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use crate::scan;
-use crate::speedtest::speedtest;
 use crate::utils::Config;
 use crate::utils::ManagerData;
 use std::error::Error;
 use std::sync::{Arc, Mutex};
-use std::thread;
 use std::time::SystemTime;
 
 slint::include_modules!();
@@ -18,10 +15,11 @@ pub fn main(config: Config) -> Result<(), Box<dyn Error>> {
     // Wrap ManagerData in Arc<Mutex<ManagerData>> for shared, mutable access
     let shared_manager = Arc::new(Mutex::new(ManagerData {
         num_led: config.num_led,
+        num_strips: config.num_strips,
         communication_mode: config.communication_mode,
         host: config.host,
         port: config.port,
-        serial_port_path: config.serial_port_path.clone(),
+        serial_port_paths: config.serial_port_paths.clone(),
         baud_rate: config.baud_rate,
         serial_read_timeout: config.serial_read_timeout,
         record_data: config.record_data,
@@ -38,25 +36,25 @@ pub fn main(config: Config) -> Result<(), Box<dyn Error>> {
         data_file_buf: None,
         esp_data_file_buf: None,
         udp_socket: None,
-        serial_port: None,
+        serial_port: Vec::new(),
         keepalive: true,
     }));
 
-    let shared_manager_speedtest = Arc::clone(&shared_manager);
-    ui.on_speedtest(move || {
-        thread::spawn(move || {
-            let mut manager = shared_manager_speedtest.lock().unwrap();
-            speedtest(&mut manager, config.num_led, 750);
-        });
-    });
+    // let shared_manager_speedtest = Arc::clone(&shared_manager);
+    // ui.on_speedtest(move || {
+    //     thread::spawn(move || {
+    //         let mut manager = shared_manager_speedtest.lock().unwrap();
+    //         speedtest(&mut manager, config.num_led, 750);
+    //     });
+    // });
 
-    let shared_manager_calibrate = Arc::clone(&shared_manager);
-    ui.on_calibrate(move || {
-        thread::spawn(move || {
-            let mut manager = shared_manager_calibrate.lock().unwrap();
-            scan::scan(config.clone(), &mut manager).unwrap();
-        })
-    });
+    // let shared_manager_calibrate = Arc::clone(&shared_manager);
+    // ui.on_calibrate(move || {
+    //     thread::spawn(move || {
+    //         let mut manager = shared_manager_calibrate.lock().unwrap();
+    //         scan::scan(config.clone(), &mut manager).unwrap();
+    //     })
+    // });
 
     ui.run()?;
 
