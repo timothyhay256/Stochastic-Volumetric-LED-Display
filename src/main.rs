dbc4567b85838ff52c7f4ddb3e0fc1af1515893e
James Hoffman <purpledungeoncrab@protonmail.com>
2025-03-03 00:02:45 -0700

    Add unstable streamlined calibration option

--- src/main.rs
+++ src/main.rs
@@ -258,7 +258,7 @@ fn main() {
     #[cfg(feature = "scan")]
     if let Some(Command::Calibrate(ref _calibrate_options)) = opts.command {
         info!("Performing calibrating");
-        scan::scan(config_holder.clone(), &manager).expect("failure");
+        scan::scan(config_holder.clone(), &manager, false).expect("failure");
     }
 
     // led_manager::set_color(&mut manager, 1, 255, 255, 255);
