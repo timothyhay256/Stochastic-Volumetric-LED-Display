8e031a40f87c2f6cd1e2278ba7669f57f2338948
James Hoffman <purpledungeoncrab@protonmail.com>
2025-06-29 17:05:53 -0600

    Add option to convert position file into cpp compatible format

--- src/main.rs
+++ src/main.rs
@@ -77,6 +77,9 @@ enum Command {
 
     #[options(help = "run a simple demo")]
     Demo(DemoOptions),
+
+    #[options(help = "convert an led position json into a C++ compatible constant")]
+    ConvertLedpos(ConvertLedposOptions),
 }
 
 #[derive(Debug, Options)]
@@ -84,19 +87,19 @@ struct SpeedtestOptions {}
 
 #[derive(Debug, Options)]
 struct ReadvledOptions {
-    #[options(help = "vled file to read")]
+    #[options(help = "vled file to read", required)]
     vled_file: PathBuf,
 }
 
 #[derive(Debug, Options)]
 struct SetColorOptions {
-    #[options(help = "index of LED")]
+    #[options(help = "index of LED", required)]
     n: u16,
-    #[options(help = "R value to set")]
+    #[options(help = "R value to set", required)]
     r: u8,
-    #[options(help = "G value to set")]
+    #[options(help = "G value to set", required)]
     g: u8,
-    #[options(help = "B value to set")]
+    #[options(help = "B value to set", required)]
     b: u8,
 }
 
@@ -121,10 +124,22 @@ struct GuiOptions {}
 struct DriverWizardOptions {}
 
 #[derive(Debug, Options)]
-struct DemoOptions {}
+struct DemoOptions {
+    #[options(help = "demo to run (rainbow, rainbow-loop)", required)]
+    active_demo: String,
+}
 
 #[derive(Debug, Options)]
 struct ClearOptions {}
+
+#[derive(Debug, Options)]
+struct ConvertLedposOptions {
+    #[options(help = "input JSON file", required)]
+    input: String,
+
+    #[options(help = "output file")]
+    output: Option<String>,
+}
 
 type JsonEntry = Vec<(String, (f32, f32), (f32, f32))>;
 
@@ -145,6 +160,69 @@ fn main() {
         config_path = Path::new(&opts.config);
     }
 
+    if let Some(Command::ConvertLedpos(ref convert_ledpos_options)) = opts.command {
+        let path = Path::new(&convert_ledpos_options.input);
+
+        info!(
+            "Converting {} into an C++ compatible data format",
+            path.display()
+        );
+
+        if path.exists() {
+            let mut pos_file = match File::open(path) {
+                Ok(file) => file,
+                Err(e) => {
+                    panic!("Could not read {:?}: {}", path.display(), e)
+                }
+            };
+
+            let mut file_contents = String::new();
+            match pos_file.read_to_string(&mut file_contents) {
+                Ok(_) => {}
+                Err(e) => {
+                    panic!("Could not read position file {}: {}", path.display(), e)
+                }
+            };
+
+            let json: JsonEntry = match serde_json::from_str(&file_contents) {
+                Ok(json) => json,
+                Err(e) => {
+                    panic!(
+                        "{} contains invalid or incomplete calibration data: {}",
+                        path.display(),
+                        e
+                    )
+                }
+            };
+
+            let mut output_string = String::new();
+
+            let len = json.len();
+
+            output_string.push_str(&format!(
+                "static constexpr std::array<std::array<int16_t, 2>, {len}> coords = {{{{"
+            ));
+
+            for (index, entry) in json.iter().enumerate() {
+                output_string.push_str(&format!(
+                    "{{{}, {}}}, {{{}, {}}}",
+                    entry.1 .0, entry.1 .1, entry.2 .0, entry.2 .1
+                ));
+
+                if index == len - 1 {
+                    output_string.push_str("}};")
+                } else {
+                    output_string.push_str(", ");
+                }
+            }
+
+            info!("{output_string}");
+        } else {
+            error!("{} not found", path.display());
+        }
+        return;
+    }
+
     let config_load_result = utils::load_validate_conf(config_path);
 
     let (manager, unity_options, config_holder) = (
@@ -152,12 +230,6 @@ fn main() {
         config_load_result.1,
         config_load_result.2,
     );
-
-    // ctrlc::set_handler(move || {
-    //     handler_manager.lock().unwrap().keepalive = false;
-    //     process::exit(0);
-    // })
-    // .expect("Error setting Ctrl-C handler");
 
     if let Some(Command::Speedtest(ref _speedtest_options)) = opts.command {
         info!("Performing speedtest...");
@@ -240,7 +312,7 @@ fn main() {
             set_color_options.g,
             set_color_options.b,
         );
-    } else if let Some(Command::Demo(ref _demo_options)) = opts.command {
+    } else if let Some(Command::Demo(ref demo_options)) = opts.command {
         info!("Running demo!");
 
         let mut pos_file = match File::open(unity_options.unity_position_files[0].clone()) {
@@ -276,10 +348,20 @@ fn main() {
             }
         };
 
-        loop {
-            demo::rainbow(&manager, &json, 80.0, 50.0, false, demo::Axis::X, true);
-            demo::rainbow(&manager, &json, 50.0, 50.0, false, demo::Axis::Y, true);
-            demo::rainbow(&manager, &json, 80.0, 50.0, false, demo::Axis::Z, true);
+        match demo_options.active_demo.to_lowercase().as_str() {
+            "rainbow-loop" => loop {
+                demo::rainbow(&manager, &json, 80.0, 50.0, false, demo::Axis::X, true);
+                demo::rainbow(&manager, &json, 50.0, 50.0, false, demo::Axis::Y, true);
+                demo::rainbow(&manager, &json, 80.0, 50.0, false, demo::Axis::Z, true);
+            },
+            "rainbow" => {
+                demo::rainbow(&manager, &json, 80.0, 50.0, false, demo::Axis::X, true);
+                demo::rainbow(&manager, &json, 50.0, 50.0, false, demo::Axis::Y, true);
+                demo::rainbow(&manager, &json, 80.0, 50.0, false, demo::Axis::Z, true);
+            }
+            option => {
+                error!("Invalid option {option}");
+            }
         }
     } else if let Some(Command::Clear(ref _clear_options)) = opts.command {
         for n in 0..config_holder.num_led {
